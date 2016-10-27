23462426df389ba344ae988b569b40193cca1d44
Boris Sazonov <sazonov@spb.gs.ru>
2016-10-27 19:01:51 +0300

    Added simple ansible configuration

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+Fail() {
+	echo $@
+	exit 1
+}
+
+Try() {
+	"$@" || Fail "$* failed!"
+}
+
+Try sudo apt-add-repository ppa:ansible/ansible
+Try sudo apt-get update
+Try sudo apt-get install -y git ansible
+Try mkdir -p ~/Projects/
+Try git clone https://github.com/bo-on-software/ansible-config.git ~/Projects/ansible-config
+Try cd ~/Projects/ansible-config
+Try ansible-playbook -v -i hosts main.yml
+
